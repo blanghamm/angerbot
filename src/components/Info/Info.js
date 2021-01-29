60e612f6277812a9e792abc7f3e3ee89839254ae
Ben Langham <langham1291@outlook.com>
2021-01-29 08:54:38 +0000

    updated info tab

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -5,18 +5,22 @@ const Info = () => {
     <div className='info-container'>
       <div className='info-inner-container'>
         <h2 className='info-title'>Info</h2>
-        <p className='info-para'>
+        <p className='info-para' style={{ lineHeight: '1.2' }}>
           The initial idea surrounding this concept was to allow users some
           therapeutic outburst rather than taking that anger or frustration out
           on person or animal.
+          <br />
           <br />
           As I researched this concept further I found a number of articles
           talking about children using smart devices, Alexa and Google Assistant
           for example. How these devices are of course programmed to listen to
           your every command and do not require manors, nor can they answer in a
-          similar way to user input. This chat bot was created using React for
-          the frontend and using IBM Watson Assistant for the language
-          processing. It also utilises, Node-Red for flow mapping.
+          similar way to user input.
+          <br />
+          <br />
+          This chat bot was created using React for the frontend and using IBM
+          Watson Assistant for the language processing. It also utilises,
+          Node-Red for flow mapping.
         </p>
       </div>
     </div>
